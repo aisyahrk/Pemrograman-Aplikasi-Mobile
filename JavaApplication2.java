c6636f7abdf048743309854df5f3ebe1e084cec3
aisyahrk <43089460+aisyahrk@users.noreply.github.com>
2018-09-08 18:08:37 +0700

    Add files via upload

---
+++ JavaApplication2.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication2;
+
+/**
+ *
+ * @author Aisyah
+ */
+public class JavaApplication2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int s = 4;
+        for (int i=1; i<=s; i++){
+            for (int a=1; a<=i; a++){
+                System.out.print("*");
+            }
+        System.out.println();
+        }
+    }
+    
+}
